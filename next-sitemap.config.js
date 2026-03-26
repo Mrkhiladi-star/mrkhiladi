3474a50a2c6a23b8e8e70a9488aaff5b88ec5486
mrkhiladi <sramu1090@gmail.com>
2026-03-27 02:37:37 +0530

    After Adding xml for google console

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://mrkhiladi.app.vercel.app',
+  generateRobotsTxt: true,
+};
--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+
 };
 
 module.exports = nextConfig;
